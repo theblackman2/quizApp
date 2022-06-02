8d7cc20864a501a45821074fbfee27951c848c4b
theblackman2 <paskasonga@gmail.com>
2022-06-02 11:08:47 +0100

    Refactor: Mooved the quitBtn event

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
-// Import all functions 
+
 import { getUserInfos, validateForm, showScore, askQuestion, TimeAnimation, checkAnswer, shurffleQuestions, fluidAnimation, SetTimeOut, hideContainer, showContainer } from "./functions.js"
 
-// Import all constants 
 import { questions, userInfosForm, welcomeArea, questionArea, finishedArea, questionsForm, formAnswers, timer, next, quit, showTime, parentTimeShow } from "./constants.js";
 
 
@@ -11,6 +10,7 @@ let animation
 let timeOut
 let question
 let user = null
+let shurffeQuestions
 
 
 // able the next btn after one answer choosed
@@ -23,7 +23,7 @@ for(let answer of questionsForm.elements.answer){
 }
 
 userInfosForm.addEventListener("submit", function(e){
-  let shurffeQuestions = shurffleQuestions(questions)
+  shurffeQuestions = shurffleQuestions(questions)
   let valid = validateForm(this)
   if(valid){
     user = getUserInfos(this)
@@ -34,50 +34,51 @@ userInfosForm.addEventListener("submit", function(e){
     animation = TimeAnimation(showTime)
     timeOut = SetTimeOut()
     fluidAnimation(parentTimeShow)
-    next.addEventListener("click", function(e){
-      if(this.getAttribute("disabled") == "false"){
-        fluidAnimation(parentTimeShow)
-        clearTimeout(timeOut)
-        clearInterval(animation)
-        timeOut = SetTimeOut()
-      }
-      let userChoice = document.querySelector(".form-answer input:checked")
-      if(userChoice){
-        let userAnswer = userChoice.nextElementSibling.textContent
-        questionsForm.reset()
-        if(checkAnswer(question, userAnswer)) score += 1
-        this.style.background = "rgba(2, 138, 61, .42)"
-        if(this.getAttribute("disabled") == "false"){
-          if(nbrQuestons < 15){
-            nbrQuestons += 1
-            question = askQuestion(nbrQuestons, formAnswers, shurffeQuestions)
-            animation = TimeAnimation(showTime)
-          }else{
-            hideContainer(questionArea)
-            showContainer(finishedArea)
-            showScore(user, score)
-          }
-        }
-        this.setAttribute("disabled", true)
-      }else{
-        if(this.getAttribute("disabled") == "false"){
-          if(nbrQuestons < 15){
-            nbrQuestons += 1
-            question = askQuestion(nbrQuestons, formAnswers, shurffeQuestions)
-            animation = TimeAnimation(showTime)
-          }else{
-            hideContainer(questionArea)
-            showContainer(finishedArea)
-            showScore(user, score)
-          }
-        }
-      }
-    })
   }
   e.preventDefault()
 })
 
-quit.addEventListener("click", function(e){
+next.addEventListener("click", function(e){
+  if(this.getAttribute("disabled") == "false"){
+    fluidAnimation(parentTimeShow)
+    clearTimeout(timeOut)
+    clearInterval(animation)
+    timeOut = SetTimeOut()
+  }
+  let userChoice = document.querySelector(".form-answer input:checked")
+  if(userChoice){
+    let userAnswer = userChoice.nextElementSibling.textContent
+    questionsForm.reset()
+    if(checkAnswer(question, userAnswer)) score += 1
+    this.style.background = "rgba(2, 138, 61, .42)"
+    if(this.getAttribute("disabled") == "false"){
+      if(nbrQuestons < 15){
+        nbrQuestons += 1
+        question = askQuestion(nbrQuestons, formAnswers, shurffeQuestions)
+        animation = TimeAnimation(showTime)
+      }else{
+        hideContainer(questionArea)
+        showContainer(finishedArea)
+        showScore(user, score)
+      }
+    }
+    this.setAttribute("disabled", true)
+  }else{
+    if(this.getAttribute("disabled") == "false"){
+      if(nbrQuestons < 15){
+        nbrQuestons += 1
+        question = askQuestion(nbrQuestons, formAnswers, shurffeQuestions)
+        animation = TimeAnimation(showTime)
+      }else{
+        hideContainer(questionArea)
+        showContainer(finishedArea)
+        showScore(user, score)
+      }
+    }
+  }
+})
+
+quit.addEventListener("click", function(){
   clearInterval(animation)
   clearTimeout(timeOut)
   hideContainer(questionArea)
